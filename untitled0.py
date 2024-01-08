1afe9636fb179bcd22fa9d603963ac51ff335447
xianwen18 <44245193+xianwen18@users.noreply.github.com>
2024-01-08 10:03:47 +0200

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+
+# 6 lifes in total
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+import random
+
+word_list = ["ardvark", "baboon","camel"]
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+lives = 6
+end_of_game = False
+
+#Testing
+
+print(f'Pssst, the solution is {chosen_word}.')
+
+
+#Create blanks
+
+display = [] 
+for _ in range(word_length):
+    display += "_"
+    
+
+    
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+      #  print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+       # print(display)
+            
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose")
+            
+    
+  
+    print(f"{' ' .join(display)}")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win")
+        
+    print(stages[lives])
